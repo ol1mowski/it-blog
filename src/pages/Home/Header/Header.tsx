b533b60e5566963a6b4c1412d5f3bd8950172b50
Ol1mowski <olimarki123@wp.pl>
2023-12-01 10:25:37 +0100

    [header] add simple sketch to desktop nav view

--- src/pages/Home/Header/Header.tsx
+++ src/pages/Home/Header/Header.tsx
@@ -9,6 +9,12 @@ const Header = () => {
           IT-Blog
         </h2>
       </section>
+      <section className={s.header__navMenu}>
+          <ul className={s.header__navMenu__items}>
+            <li className={s.header__navMenu__items__item}>Blog’s goal</li>
+            <li className={s.header__navMenu__items__item}>About Author</li>
+          </ul>
+      </section>
       <section className={s.header__hamburgerSection}>
         <img width="45" height="45" src="https://img.icons8.com/nolan/64/1A6DFF/C822FF/menu-squared-2.png" alt="menu-squared-2" />
       </section>
