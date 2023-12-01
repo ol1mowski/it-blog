b4de944603dd3cdaa1325bd8022126bc0f7b5053
Ol1mowski <101707516+ol1mowski@users.noreply.github.com>
2023-12-01 13:51:35 +0100

    diagnostic commit

--- src/pages/Home/Header/Header.tsx
+++ src/pages/Home/Header/Header.tsx
@@ -1,6 +1,6 @@
 import s from './Header.module.scss';
 
-const Header = () => {
+const Header: React.FC = () => {
   return (
     <header className={s.header}>
       <section className={s.header__titleSection}>
@@ -10,10 +10,13 @@ const Header = () => {
         </h2>
       </section>
       <section className={s.header__navMenu}>
+        <div className={s.header__navMenu__nav}>
           <ul className={s.header__navMenu__items}>
             <li className={s.header__navMenu__items__item}>Blog’s goal</li>
             <li className={s.header__navMenu__items__item}>About Author</li>
           </ul>
+        </div>
+          <img width="20" height="20" src="https://img.icons8.com/ios/50/000000/search--v1.png" alt="search--v1"/>
       </section>
       <section className={s.header__hamburgerSection}>
         <img width="45" height="45" src="https://img.icons8.com/nolan/64/1A6DFF/C822FF/menu-squared-2.png" alt="menu-squared-2" />
