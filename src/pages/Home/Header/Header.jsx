c2d3a889861a2b05e087aab0cb9d0618ec244aae
Ol1mowski <101707516+ol1mowski@users.noreply.github.com>
2023-12-08 15:14:28 +0100

    [header] add logic to show and hide hamburger menu

--- src/pages/Home/Header/Header.jsx
+++ src/pages/Home/Header/Header.jsx
@@ -1,11 +1,48 @@
 
 import s from './Header.module.scss';
+import { useRef } from 'react';
+import { useEffect } from 'react';
 
 const Header = () => {
 
+  const showHamburgerMenu = useRef(null);
+  const hamburgerMenu = useRef(null);
+  const navigationSection = useRef(null);
+  const closeHamburgerMenu = useRef(null);
+  
+
+  useEffect(() => { 
+
+    const showMenuHandler = () => {
+      hamburgerMenu.current.style.display='block';
+    }
+
+    showHamburgerMenu.current.addEventListener('click', showMenuHandler);
+
+    return(() => {
+      showHamburgerMenu.current.removeEventListener('click', showMenuHandler);
+    })
+
+  }, [showHamburgerMenu])
+
+  useEffect(() => { 
+
+    const hideMenuHandler = () => {
+      hamburgerMenu.current.style.display='none';
+    }
+
+    closeHamburgerMenu.current.addEventListener('click', hideMenuHandler);
+
+    return(() => {
+      closeHamburgerMenu.current.removeEventListener('click', hideMenuHandler);
+    })
+
+  }, [closeHamburgerMenu])
+
+
   return (
     <header className={s.header}>
-      <section className={s.header__titleSection}>
+      <section ref={navigationSection} className={s.header__titleSection}>
         <img width="45" height="45" src="https://img.icons8.com/nolan/64/1A6DFF/C822FF/programming-flag.png" alt="programming-flag" />
         <h2 className={s.header__h2}>
           IT-Blog
@@ -20,9 +57,25 @@ const Header = () => {
         </div>
         <img width="20" height="20" src="https://img.icons8.com/ios/50/000000/search--v1.png" alt="search--v1" />
       </section>
-      <section className={s.header__hamburgerSection}>
+      <section ref={showHamburgerMenu} className={s.header__hamburgerSection}>
         <img width="45" height="45" src="https://img.icons8.com/nolan/64/1A6DFF/C822FF/menu-squared-2.png" alt="menu-squared-2" />
       </section>
+
+
+      <menu ref={hamburgerMenu} className={s.header__hamburgerMenu}>
+      <section className={s.header__titleSection}>
+        <img width="45" height="45" src="https://img.icons8.com/nolan/64/1A6DFF/C822FF/programming-flag.png" alt="programming-flag" />
+        <h2 className={s.header__h2}>
+          IT-Blog
+        </h2>
+      </section>
+
+      <section ref={closeHamburgerMenu} className={s.header__hamburgerSection}>
+      <img width="30" height="30" src="https://img.icons8.com/ios-glyphs/30/delete-sign.png" alt="delete-sign"/>
+      </section>
+
+      
+      </menu>
     </header>
   );
 };
