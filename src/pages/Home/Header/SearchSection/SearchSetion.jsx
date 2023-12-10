a6179ee2bb919566ece04c647328f07f98a5e232
Ol1mowski <olimarki123@wp.pl>
2023-12-10 16:20:52 +0100

    [header] improve UI to search section

--- src/pages/Home/Header/SearchSection/SearchSetion.jsx
+++ src/pages/Home/Header/SearchSection/SearchSetion.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import { useRef } from 'react';
 import { useEffect } from 'react';
 
+import search from '../../../../assets/search.svg';
+
 import s from '../Header.module.scss';
 
-const SearchSetion = ({searchIcon}) => {
+const SearchSetion = ({ searchIcon }) => {
 
   const searchSection = useRef(null);
   const searchWrapper = useRef(null);
@@ -51,21 +53,21 @@ const SearchSetion = ({searchIcon}) => {
 
   return (
     <section ref={searchSection} className={s.header__searchSection}>
-          <div
-            ref={searchWrapper}
-            className={s.header__searchSection__searchPlace}
-          >
-            <input
-              placeholder="Enter sth"
-              type="text"
-              name="search"
-              className={s.header__searchSection__searchPlace__inp}
-            />
-            <button className={s.header__searchSection__searchPlace__btn}>
-              Search
-            </button>
-          </div>
-        </section>
+      <div
+        ref={searchWrapper}
+        className={s.header__searchSection__searchPlace}
+      >
+        <input
+          placeholder="Enter category"
+          type="text"
+          name="search"
+          className={s.header__searchSection__searchPlace__inp}
+        />
+        <button className={s.header__searchSection__searchPlace__btn}>
+          <img width="20" height="20" src={search} alt="search--v1" />
+        </button>
+      </div>
+    </section>
   )
 }
 
