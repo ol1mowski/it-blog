eb77d03b99e9b698001138b4bfc35e08b22c67be
Ol1mowski <olimarki123@wp.pl>
2023-12-10 19:51:45 +0100

    [category section] add category section and introduce RWD to moblie version

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Home from "./pages/Home/Home"
 import {  QueryClientProvider } from '@tanstack/react-query'
 import { queryClient } from "./util/http"
+import CategorySection from "./pages/CategorySection/CategorySection"
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <QueryClientProvider client={queryClient}>
         <Home />
       </QueryClientProvider>
+      <CategorySection />
     </>
   )
 }
---
+++ src/pages/CategorySection/CategorySection.jsx
@@ -0,0 +1,33 @@
+import s from './CategorySection.module.scss';
+
+const CategorySection = () => {
+    return (
+        <section className={s.categoryContainer}>
+            <div className={s.categoryContainer__wrapper}>
+                <div className={s.categoryContainer__wrapper__itemCategory}>
+                    <h2 className={s.categoryContainer__wrapper__itemCategory__content}>
+                        Categories:
+                    </h2>
+                </div>
+                <div className={s.categoryContainer__wrapper__item}>
+                    <img width="50" height="50" src="https://img.icons8.com/bubbles/50/code.png" alt="code" />
+                    All
+                </div>
+                <div className={s.categoryContainer__wrapper__item}>
+                    <img width="50" height="50" src="https://img.icons8.com/clouds/50/react.png" alt="react" />
+                    React
+                </div>
+                <div className={s.categoryContainer__wrapper__item}>
+                    <img width="50" height="50" src="https://img.icons8.com/fluency/50/imac-settings.png" alt="imac-settings" />
+                    Web Development
+                </div>
+                <div className={s.categoryContainer__wrapper__item}>
+                    <img width="50" height="50" src="https://img.icons8.com/clouds/50/apple-tips.png" alt="apple-tips" />
+                    Learning Tips
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default CategorySection
