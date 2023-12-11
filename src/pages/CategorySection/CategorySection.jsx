5bcff09f35b872183677dd153049fa75d7ecb0b8
Ol1mowski <olimarki123@wp.pl>
2023-12-11 05:50:18 +0100

    [categorySection] style: improve RWD to desktop devices

--- src/pages/CategorySection/CategorySection.jsx
+++ src/pages/CategorySection/CategorySection.jsx
@@ -10,19 +10,19 @@ const CategorySection = () => {
                     </h2>
                 </div>
                 <div className={s.categoryContainer__wrapper__item}>
-                    <img width="50" height="50" src="https://img.icons8.com/bubbles/50/code.png" alt="code" />
+                    <img className={s.categoryContainer__wrapper__item__img} width="50" height="50" src="https://img.icons8.com/bubbles/50/code.png" alt="code" />
                     All
                 </div>
                 <div className={s.categoryContainer__wrapper__item}>
-                    <img width="50" height="50" src="https://img.icons8.com/clouds/50/react.png" alt="react" />
+                    <img className={s.categoryContainer__wrapper__item__img} width="50" height="50" src="https://img.icons8.com/clouds/50/react.png" alt="react" />
                     React
                 </div>
                 <div className={s.categoryContainer__wrapper__item}>
-                    <img width="50" height="50" src="https://img.icons8.com/fluency/50/imac-settings.png" alt="imac-settings" />
+                    <img className={s.categoryContainer__wrapper__item__img} width="50" height="50" src="https://img.icons8.com/fluency/50/imac-settings.png" alt="imac-settings" />
                     Web Development
                 </div>
                 <div className={s.categoryContainer__wrapper__item}>
-                    <img width="50" height="50" src="https://img.icons8.com/clouds/50/apple-tips.png" alt="apple-tips" />
+                    <img className={s.categoryContainer__wrapper__item__img} width="50" height="50" src="https://img.icons8.com/clouds/50/apple-tips.png" alt="apple-tips" />
                     Learning Tips
                 </div>
             </div>
