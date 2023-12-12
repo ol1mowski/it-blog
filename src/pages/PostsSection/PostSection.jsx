586abc8caf18be704b2008059e9739ea83acbcae
Ol1mowski <101707516+ol1mowski@users.noreply.github.com>
2023-12-12 06:26:09 +0100

    [PostsSection] fetching data: add fetching posts from DB

--- src/pages/PostsSection/PostSection.jsx
+++ src/pages/PostsSection/PostSection.jsx
@@ -1,11 +1,34 @@
+import { fetchPosts, getImageUrl } from "../../util/http";
 import Post from "./Post/Post";
 import s from "./PostSection.module.scss";
+import { useQuery } from "@tanstack/react-query";
 
 const PostSection = () => {
+  const { data } = useQuery({
+    queryKey: ["posts"],
+    queryFnL: fetchPosts,
+  });
+
+  const { data: imageData, status: imageStatus } = useQuery({
+    queryKey: ["images"],
+    queryFn: getImageUrl,
+    gcTime: 100000,
+    staleTime: 100000,
+  });
+
+  const POSTS_DATA = data || [];
+
   return (
     <main className={s.postsContainer}>
       <section className={s.postsContainer__postsWrapper}>
-       <Post />
+        {POSTS_DATA.map((item) => (
+          <Post
+            key={item.id}
+            category={item.category}
+            title={item.title}
+            description={item.description}
+          />
+        ))}
       </section>
     </main>
   );
