c6f27eabc8f3a5cabc39f42e4f6a751ecbec655b
Ol1mowski <101707516+ol1mowski@users.noreply.github.com>
2023-12-17 21:06:06 +0100

    [footer] style: improve UI to footer-nav section to mobile devices

--- src/pages/Footer/Footer.jsx
+++ src/pages/Footer/Footer.jsx
@@ -11,10 +11,60 @@ const Footer = () => {
             src="https://img.icons8.com/nolan/64/1A6DFF/C822FF/programming-flag.png"
             alt="programming-flag"
           />
-          <h3 className={s.footerContainer__name__wrapper__h3}>It-blog for programmers.com</h3>
+          <h3 className={s.footerContainer__name__wrapper__h3}>
+            It-blog for programmers.com
+          </h3>
         </div>
       </section>
-      <section className={s.footerContainer__navigation}></section>
+      <section className={s.footerContainer__navigation}>
+        <div className={s.footerContainer__navigation__wrapper}>
+          <div className={s.footerContainer__navigation__wrapper__nav}>
+            <h3 className={s.footerContainer__navigation__wrapper__nav__h3}>
+              Navigation
+            </h3>
+            <ul className={s.footerContainer__navigation__wrapper__nav__items}>
+              <li
+                className={
+                  s.footerContainer__navigation__wrapper__nav__items__item
+                }
+              >
+                Home page
+              </li>
+              <li
+                className={
+                  s.footerContainer__navigation__wrapper__nav__items__item
+                }
+              >
+                Recommended courses
+              </li>
+              <li
+                className={
+                  s.footerContainer__navigation__wrapper__nav__items__item
+                }
+              >
+                Books for programmer
+              </li>
+              <li
+                className={
+                  s.footerContainer__navigation__wrapper__nav__items__item
+                }
+              >
+                About Author
+              </li>
+            </ul>
+          </div>
+          <div className={s.footerContainer__navigation__wrapper__sources}>
+            <div className={s.footerContainer__navigation__wrapper__sources__source}>
+            <img width="50" height="50" src="https://img.icons8.com/color/50/youtube-play.png" alt="youtube-play"/>
+              <p className={s.footerContainer__navigation__wrapper__sources__source__content}>You Tube</p>
+            </div>
+            <div className={s.footerContainer__navigation__wrapper__sources__source}>
+            <img width="50" height="50" src="https://img.icons8.com/clouds/50/contact-card.png" alt="contact-card"/>
+              <p className={s.footerContainer__navigation__wrapper__sources__source__content}>Contact</p>
+            </div>
+          </div>
+        </div>
+      </section>
     </footer>
   );
 };
