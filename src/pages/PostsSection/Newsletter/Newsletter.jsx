0e65587bc00b78f6593e5b123cc2bdfbae3c4e2e
Ol1mowski <101707516+ol1mowski@users.noreply.github.com>
2023-12-19 06:02:35 +0100

    [Newsletter] email validate: add simple email validation for newsletters emails

--- src/pages/PostsSection/Newsletter/Newsletter.jsx
+++ src/pages/PostsSection/Newsletter/Newsletter.jsx
@@ -1,8 +1,43 @@
 import s from "./Newsletter.module.scss";
 
 import img from "../../../assets/technology.jpg";
+import { useEffect, useRef, useState } from "react";
 
 const Newsletter = () => {
+  const newsInp = useRef(null);
+  const newsBtn = useRef(null);
+
+  const [isValidate, setIsValidate] = useState(false);
+  const [errorInfo, setErrorInfo] = useState("");
+
+  useEffect(() => {
+    const validInputHandler = () => {
+      const newsEmail = newsInp.current.value;
+
+      if (newsEmail.length >= 8 && newsEmail.indexOf("@") !== -1) {
+        setIsValidate(true);
+        setErrorInfo("");
+        newsInp.current.classList.remove(s.unvalid);
+        newsInp.current.value = "";
+        newsInp.current.classList.add(s.valid);
+      } else {
+        setIsValidate(false);
+        if (newsEmail.indexOf("@") === -1) {
+          setErrorInfo("Syntaxt email address");
+        } else if (newsEmail.length < 8) {
+          setErrorInfo("email must be at least 8 characters");
+        }
+        newsInp.current.classList.add(s.unvalid);
+      }
+    };
+
+    newsBtn.current.addEventListener("click", validInputHandler);
+
+    return () => {
+      newsBtn.current.removeEventListener("click", validInputHandler);
+    };
+  }, [newsBtn]);
+
   return (
     <section className={s.newsletterContainer}>
       <div className={s.newsletterContainer__wrapper}>
@@ -26,22 +61,39 @@ const Newsletter = () => {
           </li>
         </ul>
         <div className={s.newsletterContainer__wrapper__inputSection}>
-          <div className={s.newsletterContainer__wrapper__inputSection__inpWrapper}>
+          <div
+            className={s.newsletterContainer__wrapper__inputSection__inpWrapper}
+          >
             <input
-              className={s.newsletterContainer__wrapper__inputSection__inpWrapper__inp}
+              className={
+                s.newsletterContainer__wrapper__inputSection__inpWrapper__inp
+              }
               type="text"
               name="email"
+              ref={newsInp}
               placeholder="Enter your email"
             />
             <button
-              className={s.newsletterContainer__wrapper__inputSection__inpWrapper__btn}
+              ref={newsBtn}
+              className={
+                s.newsletterContainer__wrapper__inputSection__inpWrapper__btn
+              }
             >
               sign up
             </button>
           </div>
-          <div className={s.newsletterContainer__wrapper__inputSection__infoWrapper}>
+          {errorInfo ? <p>{errorInfo}</p> : null}
+          {isValidate ? <p>Your email has been added to Newsletter!</p> : null}
+
+          <div
+            className={
+              s.newsletterContainer__wrapper__inputSection__infoWrapper
+            }
+          >
             <span
-              className={s.newsletterContainer__wrapper__inputSection__infoWrapper__content}
+              className={
+                s.newsletterContainer__wrapper__inputSection__infoWrapper__content
+              }
             >
               Enter your Email to sign up for the free newsletter
             </span>
