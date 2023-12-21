cb6cab5065f17bf08fda6c28045eccff73b3c9ea
Ol1mowski <101707516+ol1mowski@users.noreply.github.com>
2023-12-21 12:09:48 +0100

    [Slider] refactoring: refactoring slider code

--- src/pages/Home/Slider/SliderComponent/SliderComponent.jsx
+++ src/pages/Home/Slider/SliderComponent/SliderComponent.jsx
@@ -3,23 +3,11 @@ import s from "../Slider.module.scss";
 import { Link } from "react-router-dom";
 
 const SliderComponent = memo(({ data }) => {
-  const slide1 = useRef(null);
-  const slide2 = useRef(null);
-  const slide3 = useRef(null);
-
-  const [category, setCategory] = useState(data[0].category);
-  const [title, setTitle] = useState(data[0].title);
-  const [description, setDescription] = useState(data[0].description);
-  const [image, setImage] = useState(data[0].image);
-
+  const slidesRefs = Array.from({ length: 3 }, () => useRef(null));
   const [index, setIndex] = useState(0);
 
   const changeSlide = () => {
-    if (index >= 2) {
-      setIndex(0);
-    } else if (index >= 0) {
-      setIndex((prev) => (prev + 1) % data.length);
-    }
+    setIndex((prev) => (prev + 1) % data.length);
   };
 
   useEffect(() => {
@@ -27,27 +15,26 @@ const SliderComponent = memo(({ data }) => {
       setIndex(index);
     };
 
-    const slides = [slide1, slide2, slide3];
+    const attachEventListeners = () => {
+      slidesRefs.forEach((slide, index) => {
+        const changeSlideHandler = () => handleSlideChange(index);
+        slide.current.addEventListener("click", changeSlideHandler);
+      });
+    };
 
-    slides.forEach((slide, index) => {
-      const changeSlideHandler = () => handleSlideChange(index);
-      slide.current.addEventListener("click", changeSlideHandler);
-    });
-
-    return () => {
-      slides.forEach((slide, index) => {
+    const detachEventListeners = () => {
+      slidesRefs.forEach((slide, index) => {
         const changeSlideHandler = () => handleSlideChange(index);
         slide.current.removeEventListener("click", changeSlideHandler);
       });
     };
-  }, [slide1, slide2, slide3]);
 
-  useEffect(() => {
-    setCategory(data[index].category);
-    setDescription(data[index].description);
-    setTitle(data[index].title);
-    setImage(data[index].image);
-  }, [index, data]);
+    attachEventListeners();
+
+    return () => {
+      detachEventListeners();
+    };
+  }, [slidesRefs]);
 
   useEffect(() => {
     const intervalId = setInterval(changeSlide, 5000);
@@ -55,71 +42,41 @@ const SliderComponent = memo(({ data }) => {
     return () => clearInterval(intervalId);
   }, [index, data]);
 
+  const renderImage = () => (
+    <img
+      src={data[index].image}
+      draggable="false"
+      loading="lazy"
+      alt="post image"
+      className={s.sliderContainer__image__img}
+    />
+  );
+
   return (
     <>
       <main className={s.sliderContainer}>
         <div className={s.sliderContainer__image}>
-          {index === 0 ? (
-            <img
-              src={image}
-              draggable="false"
-              loading="lazy"
-              alt="post image"
-              className={s.sliderContainer__image__img}
-            />
-          ) : null}
-          {index === 1 ? (
-            <img
-              src={image}
-              draggable="false"
-              loading="lazy"
-              alt="post image"
-              className={s.sliderContainer__image__img}
-            />
-          ) : null}
-          {index === 2 ? (
-            <img
-              src={image}
-              draggable="false"
-              loading="lazy"
-              alt="post image"
-              className={s.sliderContainer__image__img}
-            />
-          ) : null}
+          {renderImage()}
         </div>
         <div className={s.sliderContainer__content}>
-          <p className={s.sliderContainer__content__category}>{category}</p>
-          <h2 className={s.sliderContainer__content__title}>{title}</h2>
-          <h3 className={s.sliderContainer__content__text}>{description}</h3>
-          <Link to={`/${category}`}>
-             <p className={s.sliderContainer__content__read}>Read more</p></Link>
+          <p className={s.sliderContainer__content__category}>{data[index].category}</p>
+          <h2 className={s.sliderContainer__content__title}>{data[index].title}</h2>
+          <h3 className={s.sliderContainer__content__text}>{data[index].description}</h3>
+          <Link to={`/${data[index].category}`}>
+            <p className={s.sliderContainer__content__read}>Read more</p>
+          </Link>
         </div>
       </main>
       <section className={s.sliderContainer__sliderDots}>
-        <div
-          ref={slide1}
-          className={
-            index === 0
-              ? `${s.sliderContainer__sliderDots__dot} ${s.sliderContainer__sliderDots__dot__active}`
-              : `${s.sliderContainer__sliderDots__dot}`
-          }
-        ></div>
-        <div
-          ref={slide2}
-          className={
-            index === 1
-              ? `${s.sliderContainer__sliderDots__dot} ${s.sliderContainer__sliderDots__dot__active}`
-              : `${s.sliderContainer__sliderDots__dot}`
-          }
-        ></div>
-        <div
-          ref={slide3}
-          className={
-            index === 2
-              ? `${s.sliderContainer__sliderDots__dot} ${s.sliderContainer__sliderDots__dot__active}`
-              : `${s.sliderContainer__sliderDots__dot}`
-          }
-        ></div>
+        {slidesRefs.map((slideRef, i) => (
+          <div
+            key={i}
+            ref={slideRef}
+            className={`${s.sliderContainer__sliderDots__dot} ${
+              index === i ? s.sliderContainer__sliderDots__dot__active : ""
+            }`}
+          ></div>
+        ))}
       </section>
     </>
   );
