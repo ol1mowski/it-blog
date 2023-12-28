5296ce6c8bb7cc3b3b25f271cddf9ba34561683f
Ol1mowski <101707516+ol1mowski@users.noreply.github.com>
2023-12-28 21:15:37 +0100

    [categoryNoFound] UI: explain category no found error (add description with available categories)

--- src/Components/UI/CategoryNoFound/CategoryNoFound.jsx
+++ src/Components/UI/CategoryNoFound/CategoryNoFound.jsx
@@ -4,6 +4,8 @@ const CategoryNoFound = () => {
   return (
     <div className={s.container}>
       <h1 className={s.container__header}>[-] Category no found</h1>
+      <h2 className={s.container__header}>Available: </h2>
+      <h3 className={s.container__header}>React, Web Development, Learn Tips</h3>
     </div>
   );
 };
