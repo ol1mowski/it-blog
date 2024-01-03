3e6652871986deafb71fcc206fbf0b87085ef1c5
Ol1mowski <101707516+ol1mowski@users.noreply.github.com>
2024-01-03 06:27:06 +0100

    [footer] refactoring: replace footer files to Home catalog

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import CategorySection from "../CategorySection/CategorySection";
 import PostSection from "../PostsSection/Posts/PostSection";
 import Slider from "../Home/Slider/Slider";
-import Footer from "../Footer/Footer";
+import Footer from '../Home/Footer/Footer';
 
 const HomePage = () => {
   return (
