ee6a7def6626b574894cdb4249ab244a0008fc31
Ol1mowski <101707516+ol1mowski@users.noreply.github.com>
2024-01-05 18:41:27 +0100

    [SearchSection] refacoring: refactoring search section component by extract component to other file

--- src/pages/Home/Header/SearchSection/SearchSetion.jsx
+++ src/pages/Home/Header/SearchSection/SearchSetion.jsx
@@ -2,10 +2,8 @@ import { useContext, useRef } from "react";
 
 import { useNavigate } from "react-router-dom";
 
-import search from "../../../../assets/search.svg";
-
-import s from "../Header.module.scss";
 import SearchCategoryContext from "../../../../Context/SearchCategoryContext";
+import SearchComponent from "./SearchComponent/SearchComponent";
 
 const SearchSetion = () => {
   const navigate = useNavigate();
@@ -50,41 +48,15 @@ const SearchSetion = () => {
   };
 
   return (
-    <>
-      <img
-        onClick={showSearchSectionHandler}
-        className={s.header__navMenu__search}
-        width="20"
-        height="20"
-        src="https://img.icons8.com/ios/50/000000/search--v1.png"
-        alt="search--v1"
-      />
-      <section
-        onClick={hideSearchSectionHandler}
-        ref={searchSection}
-        className={s.header__searchSection}
-      >
-        <div
-          onClick={unBlurryBackgroundHandler}
-          className={s.header__searchSection__searchPlace}
-        >
-          <input
-            ref={searchCategoryInput}
-            onKeyDown={categoryKeyHandler}
-            placeholder="Enter category"
-            type="text"
-            name="search"
-            className={s.header__searchSection__searchPlace__inp}
-          />
-          <button
-            onClick={categoryClickHandler}
-            className={s.header__searchSection__searchPlace__btn}
-          >
-            <img width="20" height="20" src={search} alt="search--v1" />
-          </button>
-        </div>
-      </section>
-    </>
+    <SearchComponent
+      showSearchSectionHandler={showSearchSectionHandler}
+      hideSearchSectionHandler={hideSearchSectionHandler}
+      unBlurryBackgroundHandler={unBlurryBackgroundHandler}
+      categoryClickHandler={categoryClickHandler}
+      categoryKeyHandler={categoryKeyHandler}
+      searchSection={searchSection}
+      searchCategoryInput={searchCategoryInput}
+    />
   );
 };
 
